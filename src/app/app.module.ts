528edf55cc578b408dcfd5bf47ed7a0f34a7bbe5
James Schwartzkopf <james.schwartzkopf@eagletg.com>
2019-08-11 17:38:38 -0500

    Reproduce issue with matMenu yPosition and Ivy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
